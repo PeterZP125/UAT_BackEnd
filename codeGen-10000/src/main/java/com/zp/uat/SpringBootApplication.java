fb6eba46dc72235c63d300d1ba2e30939c9e8500
peter <appleskings@163.com>
2023-10-27 11:21:00 +0800

    项目初始化

---
+++ codeGen-10000/src/main/java/com/zp/uat/SpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.zp.uat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author PeterZP
+ * @create 2023-10-27 11:18
+ */
+@SpringBootApplication
+public class CodeGenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGenApplication.class, args);
+    }
+}
